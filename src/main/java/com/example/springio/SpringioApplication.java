94c64cea46f59e59870c45405fdd5a8c774a50ee
ttmttm <ttmttm@users.noreply.github.com>
2018-02-10 23:21:25 -0500

    changed to Command Line Runner

--- src/main/java/com/example/springio/SpringioApplication.java
+++ src/main/java/com/example/springio/SpringioApplication.java
@@ -1,12 +1,20 @@
 package com.example.springio;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringioApplication {
+public class SpringioApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("this is a CLI project");
+		
 	}
 }
